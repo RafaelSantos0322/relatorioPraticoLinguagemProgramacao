bd181921685dd1926a26ea56c56532709acd60db
Luiz Santos <124068652+RafaelSantos0322@users.noreply.github.com>
2024-10-02 16:52:14 -0300

    Update imc.py
    Atualizando dados

--- imc.py
+++ imc.py
@@ -1,14 +1,23 @@
 import math 
+
+# Começamos dando mensagem de Boas vindas Ao Usuário
 print("Bem-vind@ a calculadora de IMC(Índice de Massa Corpórea)")
 
-peso = input("Para começar digite seu peso  ")
+# Logo após criamos as váriaveis que irão receber os dados dígitados pelo usuário
+peso = float(input("Para começar digite seu peso  "))
+altura = float(input("Agora digite sua altura ex.: 2.10 "))
 
-altura = input("Agora digite sua altura  ")
-imc = peso / ( math.pow(altura, 2))
-
+# Aqui fazemos o  tratamamtento dos valores digitados verificando se é maior que 0
+if peso > 0 and altura > 0:
+     imc = peso / ( math.pow(altura, 2)) 
+     
+# Criamos uma váriavel níveis que recebe um array com níveis de peso
 niveis = ["Magresa Grave", "Magresa Moderada", "Magresa Leve", "Peso Normal", "Sobrepeso", "Obesidade Grau I", "Obesidade Grau II", "Obesidade Morbida"]
+
+#  Criamos uma váriavel que formata o  texto de acordo com seu imc e nível
 textoFormatado = f"Baseado no seu IMC de {round(imc, 2)} você está no nivel "
 
+# Aqui fazemos a verificação através de estruturas de controle para determinar em qual nível a pessoa está enquadrada
 if imc < 16:
      print( textoFormatado + niveis[0] )
      print("Procure um médico!")
